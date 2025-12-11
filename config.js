cec9c03aed7ee22f01b1cfa0a26c285b2f4ae31c
ryoryo7 <ryotaro.hoso@gmail.com>
2025-12-12 07:05:39 +0900

    GAS更新

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Form endpoint configuration
 // 右リンクの、「メール送信」スクリプトを参照。https://script.google.com/home?hl=ja 
 window.JBOUNDEX_CONFIG = {
-    formAction: "https://script.google.com/macros/s/AKfycbyqNbOKGMoW_oIHj-wAGaDP46Bv2YdAo-ADn8Ag3KcXHrPZHucBi2VNkL2tkeb5Rjgm/exec"
+    formAction: "https://script.google.com/macros/s/AKfycbzza4c1FQFb9WRz2KA_p2c7ZjMxKIbTR3ZmfPUO1VkyXppeuTqAXLKJXnXvS8NWfd1l/exec"
 };
