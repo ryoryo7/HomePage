f85de4b3a97c181a5b66f1ef7a5ab25aa917c67f
ryoryo7 <ryotaro.hoso@gmail.com>
2025-12-11 12:23:00 +0900

    初期設定

---
+++ config.js
@@ -0,0 +1,5 @@
+// Form endpoint configuration
+// 右リンクの、「メール送信」スクリプトを参照。https://script.google.com/home?hl=ja 
+window.JBOUNDEX_CONFIG = {
+    formAction: "https://script.google.com/macros/s/AKfycbxEE5DFTc88ftCCLm_ge80ZIi_zUEO_H-osDyWIvVY3rDOaVXnfflBi3teCmn_isass/exec"
+};
